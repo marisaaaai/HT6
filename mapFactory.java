792011275215d784797afbaa700501ee889f6a99
majomr <mor19145@uvg.edu.gt>
2020-03-10 21:46:40 -0600

    instancias

--- mapFactory.java
+++ mapFactory.java
@@ -10,14 +10,14 @@ import java.util.concurrent.*;
 public class mapFactory{
 	public Map getMap(int option){
 		if(option==1){
-			return new HashMap<K,V>();
+			return new HashMap<String,String>();
 		}
 		else{
 			if(option==2){
-				return new TreeMap<K,V>();
+				return new TreeMap<String,String>();
 			}
 			else{
-				return new LinkedHashMap<K,V>();
+				return new LinkedHashMap<String,String>();
 			}
 		}
 	}
